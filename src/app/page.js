47ad04d15f44ac05a3fcc97245e0fb4f046a1090
Npetr0v97 <nikolay.petrov.petrov.97@gmail.com>
2024-01-20 03:08:35 +0200

    some cleaning

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ import FactPanel from "@/components/FactPanel";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { Ubuntu } from "next/font/google";
-import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 // A different font... because why not
@@ -19,20 +18,7 @@ export default function Home() {
     // Providing the Redux store to the whole app by default, even though it is used only for the InfoPanel and FactPanel components
     <Provider store={store}>
       <main className={`${styles.main} ${ubuntu.className}`}>
-        {/* <button onClick={() => setIsVisible((prevState) => !prevState)}>
-          Click
-        </button>
-        <AnimatePresence>
-          {isVisible && (
-            <motion.div
-              className={styles.box}
-              animate={{ x: 400 }}
-              exit={{ scale: 2, opacity: 0 }}
-            >
-              Some text
-            </motion.div>
-          )}
-        </AnimatePresence> */}
+
         <Frame>
           <InfoPanel />
           <FactPanel />
